bea8214ade82fb33cb882723263b4c17b311341f
funkydiablo1998 <136699786+funkydiablo1998@users.noreply.github.com>
2023-07-19 17:11:05 +0700

    Add test coverage

---
+++ mountain_river_queen.php
@@ -0,0 +1,462 @@
+<?php 
+
+// #1
+$cure = "The Road to Recovery";
+
+// #2
+$hope = array(
+    "hope",
+    "faith",
+    "courage",
+    "determination",
+    "perseverance",
+);
+
+// #3
+echo $cure.PHP_EOL;
+
+// #4
+foreach ($hope as $characterType) {
+    echo "It takes ".$characterType." to make ".$cure.PHP_EOL;
+}
+
+// #5
+$wisdom = array(
+    "be optimistic",
+    "set goals",
+    "take action",
+    "learn from your failures",
+    "ask for help",
+);
+
+// #6
+echo PHP_EOL."Follow these steps to ".$cure.PHP_EOL;
+
+// #7
+foreach ($wisdom as $step) {
+    echo "- ".$step.PHP_EOL;
+}
+
+// #8
+$mentalState = array(
+    "think positively",
+    "believe in yourself",
+    "visualize success",
+    "maintain a good attitude",
+    "stay motivated",
+);
+
+// #9
+echo PHP_EOL."Maintain these mental states to achieve ".$cure.PHP_EOL;
+
+// #10
+foreach ($mentalState as $positive) {
+    echo "- ".$positive.PHP_EOL;
+}
+
+// #11
+$important = "Remember, achieving ".$cure." takes time, effort, and dedication.";
+
+// #12
+echo PHP_EOL.$important.PHP_EOL;
+
+// #13
+function dailyTips($tip) { 
+    echo "- ".$tip.PHP_EOL;
+}
+
+// #14
+echo PHP_EOL."Tips of the day".PHP_EOL;
+
+// #15
+$tips = array(
+    "Set time aside to accomplish your goals.",
+    "Surround yourself with people who encourage you and empower you.",
+    "Understand that you have the power to create the life you want.",
+    "Challenge yourself to stay focused and motivated.",
+);
+
+// #16 
+foreach ($tips as $tip) { 
+    dailyTips($tip);
+}
+
+// #17
+$rewards = array(
+    "gratitude",
+    "contentment",
+    "happiness",
+    "fulfillment",
+    "self-confidence",
+);
+
+// #18
+echo PHP_EOL."Rewards of ".$cure.PHP_EOL;
+
+// #19
+foreach ($rewards as $reward) {
+    echo "- ".$reward.PHP_EOL;
+}
+
+// #20
+$wordsOfEncouragement = "You have the power to take the necessary steps to ".$cure.". Don't give up, and keep going.";
+
+// #21
+echo PHP_EOL.$wordsOfEncouragement.PHP_EOL;
+
+// #22
+$advice = array(
+    "Find healthy activities and involve yourself in them.",
+    "Take breaks from your routine.",
+    "Be kind to yourself.",
+    "Learn from your mistakes and don't dwell on them.",
+    "Practice relaxation techniques.",
+);
+
+// #23
+echo PHP_EOL."Advice to aid in ".$cure.PHP_EOL;
+
+// #24
+foreach ($advice as $suggestion) {
+    echo "- ".$suggestion.PHP_EOL;
+}
+
+// #25
+$quote = "We all make mistakes, have struggles, and even regret things in our past. But you are not your mistakes, you are not your struggles, and you will learn from your regrets. Life is all about learning, growing, and improving.";
+
+// #26
+echo PHP_EOL.$quote.PHP_EOL;
+
+// #27
+$personalityTraits = array(
+    "patience",
+    "persistence",
+    "resilience",
+    "adaptability",
+    "discernment",
+);
+
+// #28
+echo PHP_EOL."These personality traits are important for ".$cure.PHP_EOL;
+
+// #29
+foreach ($personalityTraits as $trait) {
+    echo "- ".$trait.PHP_EOL;
+}
+
+// #30
+$quote2 = "To find yourself, think for yourself.";
+
+// #31
+echo PHP_EOL.$quote2.PHP_EOL;
+
+// #32
+$actions = array(
+    "Stay informed",
+    "Challenge yourself",
+    "Set healthy boundaries",
+    "Value your opinions",
+    "Seek out new experiences",
+);
+
+// #33
+echo PHP_EOL."These actions can help you along ".$cure.PHP_EOL;
+
+// #34
+foreach ($actions as $action) {
+    echo "- ".$action.PHP_EOL;
+}
+
+// #35
+$quote3 = "The journey of life is not about reaching the end, but about finding yourself along the way.";
+
+// #36
+echo PHP_EOL.$quote3.PHP_EOL;
+
+// #37
+$positiveThoughts = array(
+    "I can overcome any obstacle.",
+    "I choose to be optimistic.",
+    "I am capable of success.",
+    "I will take action today.",
+    "I trust my inner wisdom.",
+);
+
+// #38
+echo PHP_EOL."Positive thoughts to assist in ".$cure.PHP_EOL;
+
+// #39
+foreach ($positiveThoughts as $thought) {
+    echo "- ".$thought.PHP_EOL;
+}
+
+// #40
+$quote4 = "The only way to know where you're going is to know where you have been.";
+
+// #41
+echo PHP_EOL.$quote4.PHP_EOL;
+
+// #42
+$tips2 = array(
+    "Identify what has been holding you back.",
+    "Take some time to reflect on your life.",
+    "Analyze the patterns in your life.",
+    "Write a list of your strengths.",
+    "Make a list of things that bring you joy.",
+);
+
+// #43
+echo PHP_EOL."Tips to aid in reflection and self-discovery".PHP_EOL;
+
+// #44
+foreach ($tips2 as $tip2) {
+    echo "- ".$tip2.PHP_EOL;
+}
+
+// #45
+$quote5 = "You are strong, capable, and brave. You have the power to make your life what you want it to be.";
+
+// #46
+echo PHP_EOL.$quote5.PHP_EOL;
+
+// #47
+$habits = array(
+    "Create a reliable daily routine.",
+    "Set healthy boundaries.",
+    "Make time for yourself.",
+    "Prioritize self-care.",
+    "Stay connected with supportive people.",
+);
+
+// #48
+echo PHP_EOL."These habits can help you stay on ".$cure.PHP_EOL;
+
+// #49
+foreach ($habits as $habit) {
+    echo "- ".$habit.PHP_EOL;
+}
+
+// #50
+$quote6 = "You have the courage to do what is necessary in order to ".$cure.". Have faith that you can achieve your goals.";
+
+// #51
+echo PHP_EOL.$quote6.PHP_EOL;
+
+// #52
+$resources = array(
+    "therapy",
+    "support groups",
+    "mentors",
+    "books",
+    "ted talks",
+);
+
+// #53
+echo PHP_EOL."These resources can help you on ".$cure.PHP_EOL;
+
+// #54
+foreach ($resources as $resource) {
+    echo "- ".$resource.PHP_EOL;
+}
+
+// #55
+$quote7 = "We never fail to find something to be proud of about ourselves if we look hard enough.";
+
+// #56
+echo PHP_EOL.$quote7.PHP_EOL;
+
+// #57
+$perspective = array(
+    "Change your perspective.",
+    "Look at the big picture.",
+    "Focus on the present.",
+    "Learn from the past.",
+    "Look for the silver lining.",
+);
+
+// #58
+echo PHP_EOL."These changes in perspective can help you on ".$cure.PHP_EOL;
+
+// #59
+foreach ($perspective as $view) {
+    echo "- ".$view.PHP_EOL;
+}
+
+// #60
+$quote8 = "Every day is an opportunity to make progress towards ".$cure.". All you need to do is take the first step.";
+
+// #61
+echo PHP_EOL.$quote8.PHP_EOL;
+
+// #62
+$actions2 = array(
+    "Find a purpose and direction.",
+    "Take action and make a plan.",
+    "Be specific about your goals.",
+    "Be accountable for your progress.",
+    "Create a timeline to stay focused.",
+);
+
+// #63
+echo PHP_EOL."These actions will help to make progress on ".$cure.PHP_EOL;
+
+// #64
+foreach ($actions2 as $action2) {
+    echo "- ".$action2.PHP_EOL;
+}
+
+// #65
+$quote9 = "The journey may be long and difficult, but it is worth it. You will find yourself, and become stronger along the way.";
+
+// #66
+echo PHP_EOL.$quote9.PHP_EOL;
+
+// #67
+$selfCare = array(
+    "Take a break.",
+    "Find healthy outlets.",
+    "Practice self-love.",
+    "Express yourself.",
+    "Nourish your body.",
+);
+
+// #68
+echo PHP_EOL."Practice these forms of self-care to aid in ".$cure.PHP_EOL;
+
+// #69
+foreach ($selfCare as $care) {
+    echo "- ".$care.PHP_EOL;
+}
+
+// #70
+$quote10 = "Stop and enjoy the journey. Appreciate how far you have come, and be proud of yourself for taking the first steps to ".$cure.".";
+
+// #71
+echo PHP_EOL.$quote10.PHP_EOL;
+
+// #72
+$attitude = array(
+    "Be patient and persistent.",
+    "Forgive yourself and others.",
+    "Choose to be positive.",
+    "Learn from setbacks and mistakes.",
+    "Celebrate your progress.",
+);
+
+// #73
+echo PHP_EOL."These are attitudes to have while on ".$cure.PHP_EOL;
+
+// #74
+foreach ($attitude as $attitudeAdjustment) {
+    echo "- ".$attitudeAdjustment.PHP_EOL;
+}
+
+// #75
+$quote11 = "Each day may not go as planned, but that doesn't mean it can't be a day that brings you closer to ".$cure.".";
+
+// #76
+echo PHP_EOL.$quote11.PHP_EOL;
+
+// #77
+$motivation = array(
+    "Prioritize your mental and physical health.",
+    "Keep a journal and reflect on your progress.",
+    "Recognize your victories and accomplishments.",
+    "Take time to appreciate the small things.",
+    "Know that this is a process and it takes time.",
+);
+
+// #78
+echo PHP_EOL."These actions can help to stay motivated on ".$cure.PHP_EOL;
+
+// #79
+foreach ($motivation as $motivator) {
+    echo "- ".$motivator.PHP_EOL;
+}
+
+// #80
+$quote12 = "It takes great courage to look within and face the truth. This is the only way to truly heal and recover.";
+
+// #81
+echo PHP_EOL.$quote12.PHP_EOL;
+
+// #82
+$acceptance = array(
+    "Forgive yourself and accept what is.",
+    "Let go of expectations.",
+    "Accept that change can be difficult.",
+    "Be mindful of your feelings.",
+    "Be kind to yourself and others.",
+);
+
+// #83
+echo PHP_EOL."These are steps to accepting and embracing ".$cure.PHP_EOL;
+
+// #84
+foreach ($acceptance as $accept) {
+    echo "- ".$accept.PHP_EOL;
+}
+
+// #85
+$quote13 = "The most important part of recovery is knowing that you deserve it. You are worthy.";
+
+// #86
+echo PHP_EOL.$quote13.PHP_EOL;
+
+// #87
+$journey = array(
+    "Allow yourself to feel your emotions.",
+    "Be open to the changes that will come.",
+    "Give yourself permission to take a break.",
+    "Identify your needs and take care of them.",
+    "Establish healthy habits to stay on track",
+);
+
+// #88
+echo PHP_EOL."These actions will help you along the road to ".$cure.PHP_EOL;
+
+// #89
+foreach ($journey as $action3) {
+    echo "- ".$action3.PHP_EOL;
+}
+
+// #90
+$quote14 = "Stay focused on your goal and believe in yourself. You can make it to the other side, and you can make it to ".$cure.".";
+
+// #91
+echo PHP_EOL.$quote14.PHP_EOL;
+
+// #92
+$wisdom2 = array(
+    "Be true to yourself.",
+    "Trust your instincts.",
+    "Listen to your inner voice.",
+    "Follow the path that resonates with you.",
+    "Choose to make choices that will benefit you in the long run.",
+);
+
+// #93
+echo PHP_EOL."These are pieces of wisdom for the road to ".$cure.PHP_EOL;
+
+// #94
+foreach ($wisdom2 as $wise) {
+    echo "- ".$wise.PHP_EOL;
+}
+
+// #95
+$quote15 = "No matter what happens, remember that you are capable. You have the strength to continue on this path to ".$cure.".";
+
+// #96
+echo PHP_EOL.$quote15.PHP_EOL;
+
+// #97
+$relationships = array(
+    "Surround yourself with supportive people.",
+    "Build meaningful relationships.",
+    " Respect yourself and others.",
+    "Set boundaries and stick to them.",
+    "Be honest with yourself and others.",
+);
+
+// #98
+echo PHP_EOL."These actions will help
